425f57e431adad45c40ae91b3bc18bc55382d739
github username <github email address>
2021-03-06 15:47:25 -0600

    created store and added provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
 
+let reducer = ''
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider >
+<App />
+</Provider>
+
+document.getElementById('root'));
 
 
