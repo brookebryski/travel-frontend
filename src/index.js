4cbc4132a7f3b80590aa9b4042d68240cb6c3326
github username <github email address>
2021-03-08 14:08:51 -0600

    wrapped app in router index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import tripReducer from './reducers/tripReducer'
 
 import App from './App';
@@ -12,8 +13,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(tripReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-<Provider store={store} >
-<App />
+<Provider store={store}>
+  <Router> 
+    <App />
+  </Router>
 </Provider>,
 
 document.getElementById('root'));
