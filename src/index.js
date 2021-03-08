e598f59417bfc891d7f1bdbe3919131df3a806fa
github username <github email address>
2021-03-08 08:44:18 -0600

    added fetchTrips action and updtated trip reducer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import tripReducer from './reducers/tripReducer'
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(tripReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 <Provider store={store} >
