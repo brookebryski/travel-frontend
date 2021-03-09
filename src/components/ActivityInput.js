0f256f6588956a45da10cd92435067b055dbd546
github username <github email address>
2021-03-09 14:38:07 -0600

    finished up activity input form and event handlers

--- src/components/ActivityInput.js
+++ src/components/ActivityInput.js
@@ -1,15 +1,48 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
 
 class ActivityInput extends React.Component {
+
+    state = {
+        kind: 'sight',
+        location: '',
+        description: ''
+      }
+
+      handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+      }
+
+      handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addActivity(this.state, this.props.trip.id)
+        this.setState({
+          kind: 'sight',
+          location: '',
+          description: ''
+        })
+      }
 
     render() {
         return (
             <div>
-              ActivityInput
+              <form onSubmit={this.handleSubmit}>
+                <label>Activity Type:</label>
+                 <select name="kind" value={this.state.kind} onChange={this.handleChange}>
+                    <option>sight</option>
+                    <option>cuisine</option>
+                </select>
+                 <label> Location:</label>
+                <input type="text" name="location" value={this.state.location} onChange={this.handleChange}/>
+                <label> Description:</label>
+                <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+                 <input type="submit"/>
+             </form>
             </div>
         )
     }
 }
 
-export default ActivityInput 
+export default connect(null)(ActivityInput)
