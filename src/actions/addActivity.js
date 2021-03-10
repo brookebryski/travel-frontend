90897ff011cc539a9d8d15c911e6f96c5c34d9bf
github username <github email address>
2021-03-10 14:26:25 -0600

    deleting functionality is working but adding activity is not

--- src/actions/addActivity.js
+++ src/actions/addActivity.js
@@ -1,4 +1,21 @@
 export const addActivity = (activity, tripId) => {
+    return (dispatch) => {
+      fetch(`http://localhost:3000/api/v1/trips/${tripId}/activities`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(activity)
+      })
+      .then(response => response.json())
+      .then(trip => {
+        dispatch({type: 'ADD_ACTIVITY', payload: trip})
+    })
+  }
+}
+
+/*
+export const addActivity = (activity, tripId) => {
 
     return (dispatch) => {
       fetch(`http://localhost:3000/api/v1/trips/${tripId}/activities`, {
@@ -12,3 +29,4 @@ export const addActivity = (activity, tripId) => {
       .then(trip => dispatch({type: 'ADD_ACTIVITY', payload: activity}))
     }
   }
+  */
--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import {connect} from 'react-redux'
 import {deleteActivity} from '../actions/deleteActivity'
@@ -22,4 +23,31 @@ import {deleteActivity} from '../actions/deleteActivity'
  }
 
  export default connect(null, {deleteActivity})(Activities)
+ /*
 
+import React from 'react';
+import { connect } from 'react-redux';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
+import {deleteActivity} from '../actions/deleteActivity'
+
+
+const Activities = props => {
+
+  const handleDelete = (activity) => {
+    props.deleteActivity(activity.id, activity.trip_id)
+  }
+
+  return (
+    <div className='Activities'>
+      {props.activities && props.activities.length === 0 ? <h6>There aren't any activities for this location yet</h6> : <h5>Below Are Some Activities for this Location</h5>}
+      <ListGroup>
+        {props.activities && props.activities.map(activity =>
+          <ListGroupItem style={{fontSize: 'xx-large' }} key={activity.id}>{activity.location}   <Button style={{backgroundColor: 'black'}}onClick={() => handleDelete(activity)} className='DeleteButton'>Delete This Activity</Button></ListGroupItem>
+        )}
+      </ListGroup>
+    </div>
+  );
+};
+
+export default connect(null, { deleteActivity })(Activities);
+*/
