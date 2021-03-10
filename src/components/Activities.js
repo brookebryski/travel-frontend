62bc859e93bbee603221c468835c5d1447ddaa72
github username <github email address>
2021-03-10 08:52:59 -0600

    implemented delete functionality

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {deleteActivity} from '../actions/deleteActivity'
 
- const Activities = (props) => {
+ class Activities extends React.Component {
 
     state = {}
 
@@ -10,14 +10,15 @@ import {deleteActivity} from '../actions/deleteActivity'
         this.props.deleteActivity(activity.id, activity.trip_id)
       }
 
+  render () {
     return (
         <div>
-         {props.activities && props.activities.map(activity =>
+         {this.props.activities && this.props.activities.map(activity =>
          <li key={activity.id}><h3>{activity.place}:</h3>({activity.kind})<br></br> {activity.description} <button onClick={() => this.handleDelete(activity)}>Remove</button></li>
          )}
         </div>
     )
-
+  }
  }
 
  export default connect(null, {deleteActivity})(Activities)
