06a38cc335b78d6180813a6bf211a5c615ffc041
github username <github email address>
2021-03-11 14:10:42 -0600

    navbar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+ 
+import { Link } from 'react-router-dom'
+//import { Menu } from 'semantic-ui-react';
+
+//import '../App.css';
+ 
+
+
+const NavBar = () => {
+
+ 
+    return (
+
+ 
+        <div className="nav">
+          <Menu> 
+       
+
+      
+            <Link to='/' className='item'>
+                Home
+            </Link>
+
+            <Link to='/toys' className='item'>
+                View All Trips
+            </Link>
+
+            <Link to='/myStuff' className='item'>
+                View My Trips
+            </Link> 
+ 
+
+            <Link to='/about' className='item'>
+                About Us
+            </Link>
+
+   
+             
+
+        </Menu>
+        </div>
+    )
+}
+ 
+
+
+export default NavBar
