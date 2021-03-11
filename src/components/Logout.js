c1a7669175c5888ff65769e85971eedb28484acb
github username <github email address>
2021-03-11 14:08:17 -0600

    logout.js

---
+++ src/components/Logout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../actions/currentUser'
+//import {Icon } from 'semantic-ui-react';
+ 
+
+
+const Logout = ({ logout }) => {
+
+
+ 
+    return (
+        <form onSubmit={logout}>
+
+            
+           <button className="button button-logout" type="submit" value="Log Out" ><Icon name="sign out" />Log out</button>
+            
+        </form>
+    )
+}
+ 
+
+
+export default connect(null, { logout })(Logout)
