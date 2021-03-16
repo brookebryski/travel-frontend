3fffd1b41fc89bf349214cc9951e80e0f22024d8
github username <github email address>
2021-03-16 13:44:27 -0500

    edited about.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,7 @@ const Home = () => {
   <div className="Home">
      <Divider />
 
-   <p>Remember all the cool trips you had as a kid? You can view the memorable trips you had from childhood.</p>
-   <p>Claim the trips you played from the 70s, 80s, 90s ... .</p>
+   <p>Welcome to your travel journal. Here, you can keep track of places you want to go, places you have been to, and places other travelers recommend..</p>
    <img src="https://williamsonsource.com/wp-content/uploads/2020/06/Travel.jpg" alt="Travel" />
   </div>
 )
