e5a30b528ad6f33125280341e0bf50f62a0326fa
github username <github email address>
2021-03-16 14:46:10 -0500

    debugging

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,11 @@ const Home = () => {
   <div className="Home">
      <Divider />
 
+<<<<<<< HEAD
    <p>Welcome to your travel journal. Here, you can keep track of places you want to go, places you have been to, and places other travelers recommend..</p>
+=======
+   <p>Welcome to your travel journal. Here, you can keep track of places you want to go, places you have been to, and places other travelers recommend.</p>
+>>>>>>> edited home.js blurb
    <img src="https://williamsonsource.com/wp-content/uploads/2020/06/Travel.jpg" alt="Travel" />
   </div>
 )
